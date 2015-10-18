758cdb7992399d7dc42921e42435c45ab9ebe04e
Pavel Rudenkov <101dmx@gmail.com>
2015-10-19 00:36:28 +0300

    Final project. Commit 4. Logic minor changes.

--- Homework/FinalXProject/src/Main.java
+++ Homework/FinalXProject/src/Main.java
@@ -1,5 +1,15 @@
+import Threads.Thread;
+
 /**
  * Created by Pavlik on 10/18/15.
  */
-public class Main {
+public class Main extends Thread {
+
+    public static void main(String[] args) {
+
+
+        java.lang.Thread thr = new java.lang.Thread();
+
+        thr.run();
+    }
 }
