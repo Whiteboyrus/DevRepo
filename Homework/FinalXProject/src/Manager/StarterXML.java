8f7dc14122f9fe696eacff0446d6feef7a1ef22e
root <root@Pavlik-Gentoo-CLD.laptop>
2015-10-20 20:19:37 +0300

    Final project. Commit 5. new Starter class for SAX parser

--- Homework/FinalXProject/src/Manager/StarterXML.java
+++ Homework/FinalXProject/src/Manager/StarterXML.java
@@ -1,11 +1,42 @@
 package Manager;
 
+import AllParsers.SAXParser;
+import Students.Root;
+import Threads.Threads;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParserFactory;
+import java.io.File;
+import java.io.IOException;
+
 /**
  * Created by Pavlik on 10/20/15.
  */
-public class StarterXML {
+public class StarterXML extends Threads{
 
+    public StarterXML (String fileName){
 
+        super(fileName);
 
+    }
 
+    @Override
+    public void parse() {
+
+        SAXParserFactory saxFact = SAXParserFactory.newInstance();
+        SAXParser saxParser = new SAXParser();
+
+        try {
+            javax.xml.parsers.SAXParser parser = saxFact.newSAXParser();
+            parser.parse(this.getFileName(), saxParser);
+            saxParser.outList();
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
